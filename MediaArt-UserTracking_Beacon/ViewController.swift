8bdeedb897b9e18b979e8de3b7723aafe1bf83e6
Masaki Kobayashi <makky.4d6b.3f5@gmail.com>
2014-10-06 11:41:58 +0900

    add app

---
+++ MediaArt-UserTracking_Beacon/ViewController.swift
@@ -0,0 +1,55 @@
+//
+//  ViewController.swift
+//  MediaArt-UserTracking_Beacon
+//
+//  Created by Masaki Kobayashi on 2014/10/05.
+//  Copyright (c) 2014年 Masaki Kobayashi. All rights reserved.
+//
+
+import UIKit
+import CoreLocation
+import CoreBluetooth
+
+class ViewController: UIViewController, CBPeripheralManagerDelegate {
+    
+    // LocationManager
+    var myPheripheralManager:CBPeripheralManager!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // PeripheralManagerを定義.
+        myPheripheralManager = CBPeripheralManager(delegate: self, queue: nil)
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func peripheralManagerDidUpdateState(peripheral: CBPeripheralManager!) {
+        
+        // iBeaconのUUID.
+        let myProximityUUID = NSUUID(UUIDString: "AAAAAAAA-AAAA-AAAA-AAAA-AAAAAAAAAAAA")
+        
+        // iBeaconのIdentifier.
+        let myIdentifier = ""
+        
+        // Major.
+        let myMajor : CLBeaconMajorValue = 1
+        
+        // Minor.
+        let myMinor : CLBeaconMinorValue = 2
+        
+        // BeaconRegionを定義.
+        let myBeaconRegion = CLBeaconRegion(proximityUUID: myProximityUUID, major: myMajor, minor: myMinor, identifier: myIdentifier)
+        
+        // Advertisingのフォーマットを作成.
+        let myBeaconPeripheralData = myBeaconRegion.peripheralDataWithMeasuredPower(nil)
+        
+        // Advertisingを発信.
+        myPheripheralManager.startAdvertising(myBeaconPeripheralData)
+        
+    }
+}
